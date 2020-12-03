8b0d4d75602d63849ec4bc6619985e0008552302
kaydc18 <klattk@gmail.com>
2020-12-03 15:02:31 -0500

    adding movie migration and model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :director, presence: true
+  validates :release_year, presence: true
+  validates :description
+  validates :thumbs_up
+  validates :thumbs_down
 end
